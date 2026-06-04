fc4cfafe46d870fb55590205bb6eed8d485c064c
Leonardo Fuchs <fuchsleonardo01@gmail.com>
2026-06-04 02:23:14 -0300

    Feat: Modeling the User Entity.

---
+++ src/main/java/com/helpdesk/api/user/domain/User.java
@@ -0,0 +1,22 @@
+package com.helpdesk.api.user.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "users")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Builder
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+    private String profile;
+}
